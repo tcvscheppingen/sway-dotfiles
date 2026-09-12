e03c4ad468efeb39785f137ee90fe905872d143e
Thymen van Scheppingen <t.c.scheppingen@protonmail.com>
2026-09-12 21:04:35 +0200

    Added installations and fonts, moved wallpaper

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -12,4 +12,18 @@ mkdir -p "$HOME_DIR/.config"
 cp -a "$REPO_DIR/.sway/." "$HOME_DIR/.sway/"
 cp -a "$REPO_DIR/.config/." "$HOME_DIR/.config/"
 
-echo "Dotfiles copied successfully."
+mkdir -p ~/.local/share/fonts/JetBrainsMonoNerdFont
+cd ~/.local/share/fonts/JetBrainsMonoNerdFont
+
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+unzip JetBrainsMono.zip
+rm JetBrainsMono.zip
+
+fc-cache -fv
+
+echo "Installed dotfiles successfully"
+echo "Installing Neovim"
+sudo dnf install neovim
+echo "Installing Helium Browser"
+sudo dnf enable input/helium
+sudo dnf install helium-bin
